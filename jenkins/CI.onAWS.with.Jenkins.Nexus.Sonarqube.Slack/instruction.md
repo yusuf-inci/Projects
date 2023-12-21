999ac22bf627e40771df3f0c59ef382df864c1ce
yusuf-inci <yusufinci@protonmail.com>
2023-12-21 12:35:38 +0300

    added user data

--- jenkins/CI.onAWS.with.Jenkins.Nexus.Sonarqube.Slack/instruction.md
+++ jenkins/CI.onAWS.with.Jenkins.Nexus.Sonarqube.Slack/instruction.md
@@ -20,9 +20,14 @@ ssh source - myip
 80 source - myip
 80 source -jenkins-SG  
 
-
-
-## EC2 Instances
+## EC2 Instances  
+for user data: `https://github.com/hkhcoder/vprofile-project` branch: `ci-jenkins`  
+- Create ec2 for jenkins:  
+name: JenkinsServer, image: ubuntu 22.04 LTS (free tier), instance type: t2small if your instances hangs or any problem then change it to t2.medium, key-pair: vprofile-ci-key, Security Group: jenkins-SG, user data: jenkins-setup.sh, Launch İnstance  
+- Create ec2 for nexus:  
+name: NexusServer, image: in market use CentOS Stream 9 (login user:ec2-user not CentOS), instance type: t2.medium, key-pair: vprofile-ci-key, Security Group: nexus-SG, user data: nexus-setup.sh, Launch İnstance  
+- Create ec2 for sonar:  
+name: SonarServer, image: ubuntu 22.04 LTS (free tier), instance type: t2.medium, key-pair: vprofile-ci-key, Security Group: sonar-SG, user data: sonar-setup.sh, Launch İnstance  
 ## Post Installation
 ## Github Repository
 ## Build Job with Nexus Integration
---
+++ jenkins/CI.onAWS.with.Jenkins.Nexus.Sonarqube.Slack/userdata/jenkins-setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+sudo apt update
+sudo apt install openjdk-11-jdk -y
+sudo apt install maven wget unzip -y
+
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+# Docker Installation
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor 
+-o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
+gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io 
+docker-buildx-plugin docker-compose-plugin
+
+
